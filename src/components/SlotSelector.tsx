7d06c354d06a50e5026e31137ccf3489a43d331b
Metin Çelik <metincelik88@gmail.com>
2025-07-29 00:39:59 +0200

    Change color of tabs bar

--- src/components/SlotSelector.tsx
+++ src/components/SlotSelector.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Chip, Tabs, Tab } from "@mui/material";
+import { Box, Chip, Tabs, Tab, AppBar } from "@mui/material";
 import type { CharacterSlot } from "../types/SaveFile";
 
 interface SlotSelectorProps {
@@ -20,30 +20,32 @@ export const SlotSelector: React.FC<SlotSelectorProps> = ({
   };
 
   return (
-    <Tabs
-      value={currentSlot}
-      onChange={handleTabChange}
-      sx={{ borderBottom: 1, borderColor: "divider" }}
-      centered
-    >
-      {validSlots.map((slot) => (
-        <Tab
-          key={slot.name}
-          label={
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-              {slot.name}
-              <Chip
-                label={`${slot.relics.length} ${
-                  slot.relics.length === 1 ? "relic" : "relics"
-                }`}
-                size="small"
-                variant="outlined"
-              />
-            </Box>
-          }
-          sx={{ textTransform: "none" }}
-        />
-      ))}
-    </Tabs>
+    <AppBar position="static">
+      <Tabs
+        value={currentSlot}
+        onChange={handleTabChange}
+        sx={{ borderBottom: 1, borderColor: "divider" }}
+        centered
+      >
+        {validSlots.map((slot) => (
+          <Tab
+            key={slot.name}
+            label={
+              <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                {slot.name}
+                <Chip
+                  label={`${slot.relics.length} ${
+                    slot.relics.length === 1 ? "relic" : "relics"
+                  }`}
+                  size="small"
+                  variant="outlined"
+                />
+              </Box>
+            }
+            sx={{ textTransform: "none" }}
+          />
+        ))}
+      </Tabs>
+    </AppBar>
   );
 };
