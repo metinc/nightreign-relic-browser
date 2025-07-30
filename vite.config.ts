08ec27500066c164bb328f4d755b189b811cc6b1
Metin Çelik <metincelik88@gmail.com>
2025-07-30 02:06:03 +0200

    Minify and remove logs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: "esbuild",
+  },
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
   test: {
     environment: "jsdom",
     globals: true,
