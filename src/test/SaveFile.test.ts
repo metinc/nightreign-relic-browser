d588fe905d805241fc483c3aa544b676874a2ff6
Metin Çelik <metincelik88@gmail.com>
2025-08-06 23:54:04 +0200

    Add another test file

--- src/test/SaveFile.test.ts
+++ src/test/SaveFile.test.ts
@@ -24,6 +24,7 @@ const testData: TestData = [
   },
   { name: "player.sl2", slots: [{ name: "Player", relics: 651 }] },
   { name: "teru.sl2", slots: [{ name: "Teru", relics: 254 }] },
+  { name: "teru2.sl2", slots: [{ name: "Teru", relics: 284 }] },
   { name: "stluna.sl2", slots: [{ name: "ST. Luna", relics: 403 }] },
   // { name: "eonacat.sl2", slots: [{ name: "EonaCat", relics: 743 }] },
   { name: "pekzer.sl2", slots: [{ name: "Pekzer", relics: 139 }] },
